8915095897bcdc668879065f70998ed2a208dd51
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-16 21:29:53 -0300

    Consulta ao perfil de páginas funcionando.

--- plugins/facebook/data/about.js
+++ plugins/facebook/data/about.js
@@ -2,15 +2,108 @@ module.exports = () => {};
 
 module.exports.request = (id) => {
 	return {
-		version: '2.4',
+		version: '2.5',
 		method: 'GET',
 		endpoint: `${id}`,
-		fields: []
+		fields: [
+      // General
+      'about',
+      'bio',
+      'business',
+      'description',
+      'description_html',
+      'category',
+      'category_list',
+      'contact_address',
+      'current_location',
+      'general_info',
+      'emails',
+      'hours',
+      'impressum',
+      'link',
+      'name',
+      'name_with_location_descriptor',
+      'parking',
+      'phone',
+      'place_type',
+      'single_line_address',
+      'store_number',
+      'username',
+      'website',
+
+      // Flags
+      'is_always_open',
+      'is_community_page',
+      'is_permanently_closed',
+      'is_published',
+      'is_unclaimed',
+      'is_verified',
+      'verification_status',
+
+      // Picture
+      'cover',
+
+      // Bands
+      'band_interests',
+      'band_members',
+      'booking_agent',
+      'hometown',
+      'influences',
+      'press_contact',
+      'record_label',
+
+      // Company
+      'company_overview',
+      'founded',
+      'mission',
+      'products',
+
+      // Films
+      'directed_by',
+      'plot_outline',
+      'produced_by',
+      'release_date',
+      'screenplay_by',
+      'starring',
+      'studio',
+
+      // Organisations
+      'members',
+
+      // People
+      'birthday',
+      'personal_info',
+      'personal_interests',
+
+      // Pharmacies
+      'pharma_safety_info',
+
+      // Restaurants
+      'culinary_team',
+      'food_styles',
+      'general_manager',
+      'payment_options',
+      'price_range', // $ (0-10), $$ (10-30), $$$ (30-50), $$$$ (50+)
+      'public_transit',
+      'restaurant_services',
+      'restaurant_specialties',
+
+      // TV shows
+      'network',
+      'schedule',
+      'season',
+      'written_by',
+
+      // Vehicles
+      'built',
+      'features',
+      'mpg'
+    ]
 	};
 };
 
 module.exports.transform = (input) => {
 	return {
-		
+
 	};
 };
--- plugins/facebook/request/single.js
+++ plugins/facebook/request/single.js
@@ -2,6 +2,11 @@
 
 module.exports = (FB, options) => {
 	let endpoint = `/v${options.version}/${options.endpoint}`;
+
+	if (options.fields) {
+		endpoint = endpoint + '?fields=' + options.fields.join(',')
+	}
+
 	console.log(`Endpoint: ${endpoint}`);
 
 	var promise = new Promise((resolve, reject) => {
