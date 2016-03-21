31134b9f3d28df6fc98d815380ad130594e6866f
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-21 18:18:05 -0300

    Renaming library folder.

--- bin/bs-about.js
+++ bin/bs-about.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-const Facebook = require('../social/facebook')
+const Facebook = require('../lib/facebook')
 
 program
   .parse(process.argv)
