491ac8a7040032e70de26e1740b897e9c67198dd
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-21 19:51:35 -0300

    Code refactor.

--- bin/index.js
+++ bin/index.js
@@ -10,19 +10,49 @@ const Facebook = require('../lib/facebook')
 // Initialise Redis client
 var client = redis.createClient()
 
-client.on('error', (err) => {
-  console.log('Error:', err)
-})
-
-// Listen to published messages
-client.on('message', (channel, message) => {
-  try {
-    let data = JSON.parse(message)
-    console.log('Channel: ' + channel, ', Data:', data)
-  } catch (e) {
-    console.log('Channel: ' + channel, ', Message:', message)
-  }
-})
+// Setup client handlers
+client.on('error', handleErrors)
+client.on('message', listenMessages)
 
 // Subscribe to the pubsub channel
 client.subscribe(CHANNEL_NAME)
+
+/**
+ * Handle client errors.
+ * @param  {String} err Error message.
+ */
+function handleErrors (err) {
+  console.error('Error:', err)
+}
+
+/**
+ * Listen to published messages.
+ * @param  {String} channel Channel the message was delivered to.
+ * @param  {String} message Message body.
+ */
+function listenMessages (channel, message) {
+  let data = tryParseJson(message)
+
+  if (data) {
+    console.log('Channel: ' + channel, ', Data:', data)
+  } else {
+    console.log('Channel: ' + channel, ', Message:', message)
+  }
+}
+
+/**
+ * Try to parse a given string to an objected.
+ * @param  {String} str String to be parsed.
+ * @return {Object}     JSON object, if valid, otherwise false.
+ */
+function tryParseJson (str) {
+  try {
+    let data = JSON.parse(str)
+
+    if (data && typeof data === 'object' && data !== null) {
+      return data;
+    }
+  } catch (e) { }
+
+  return false
+}
